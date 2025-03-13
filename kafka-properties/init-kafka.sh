9e872b4ed7725b6ce45b3ce2d8ecb43d98ba01be
Yurii Maliuha <yu.maliuha97@gmail.com>
2025-03-13 16:39:03 +0100

    add elasticsearch sink

--- kafka-properties/init-kafka.sh
+++ kafka-properties/init-kafka.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # run below command to init kafka connect
-# docker exec -it $(docker ps -qf "name=kafka-cluster") bash -c "./init-kafka.sh"
+# docker exec -it $(docker ps -qf "name=kafka-cluster") bash -c "./properties/init-kafka.sh"
 
 kafka-topics --create --topic transactions-raw --partitions 3 --replication-factor 1 --zookeeper 127.0.0.1:2181
 cd properties
 connect-standalone worker.properties file-stream-standalone.properties
+
+# when connectors are initialized create ES sink
+# curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json" -d @properties/elasticsearch-sink.json
